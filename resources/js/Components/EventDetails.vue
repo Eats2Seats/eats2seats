a1d1d94621d33835399d081e79cc1a3fb1ee2185
devenjahnke <contact@devenjahnke.com>
2021-04-23 05:54:20 -0400

    Reservation show tweaks

---
+++ resources/js/Components/EventDetails.vue
@@ -0,0 +1,92 @@
+<template>
+    <x-label>
+        Title
+    </x-label>
+    <x-text>
+        {{ event.title }}
+    </x-text>
+    <x-divider/>
+    <div class="flex flex-row items-center justify-between">
+        <div>
+            <x-label>
+                Location
+            </x-label>
+            <x-text>
+                {{ venue.name }}
+            </x-text>
+        </div>
+        <div>
+            <a
+                :href="googleMapsLink"
+                target="_blank"
+            >
+                <x-icon-button>
+                    <MapIcon class="h-6 w-6 text-gray-500"/>
+                </x-icon-button>
+            </a>
+        </div>
+    </div>
+    <x-divider/>
+    <x-label>
+        Start
+    </x-label>
+    <x-text>
+        {{ formatDate(event.start) }}
+    </x-text>
+    <x-divider/>
+    <x-label>
+        End
+    </x-label>
+    <x-text>
+        {{ formatDate(event.end) }}
+    </x-text>
+</template>
+
+<script>
+import XLabel from '@/Components/Label';
+import XText from "@/Components/Text";
+import XDivider from "@/Components/Divider";
+import XIconButton from "@/Components/IconButton";
+import { MapIcon } from '@heroicons/vue/outline'
+export default {
+    name: 'EventDetails',
+    components: {
+        XLabel,
+        XText,
+        XDivider,
+        XIconButton,
+        MapIcon
+    },
+    props: {
+        event: Object,
+        venue: Object,
+    },
+    computed:{
+        googleMapsLink () {
+            return `https://www.google.com/maps/place/${this.venue.street}+${this.venue.city}+${this.venue.state}+
+            ${this.venue.zip}`.split(' ').join('+');
+        },
+    },
+    methods: {
+        formatDate (rawDate) {
+            let dateObj = new Date(rawDate);
+            let dateStr = dateObj.toLocaleDateString('en-US', {
+                day: 'numeric',
+                weekday: 'short',
+                month: 'long',
+                ...(new Date().getFullYear() !== dateObj.getFullYear() && { year: 'numeric'}),
+            });
+            let timeStr = dateObj.toLocaleTimeString('en-US', {
+                hour12: true,
+                hour: '2-digit',
+                minute: '2-digit',
+            });
+            return `${dateStr} @ ${timeStr}`;
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Volunteer/Reservation/Show.vue
+++ resources/js/Pages/Volunteer/Reservation/Show.vue
@@ -1,40 +1,110 @@
 <template>
     <volunteer-layout :breadcrumbs="breadcrumbs">
-        <event-card
-            :event="event"
-            :venue="venue"
-        />
-        <stand-card
-            :reservation="reservation"
-        />
+        <x-card>
+            <x-title>
+                Event
+            </x-title>
+            <x-subtitle>
+                View details about this upcoming event.
+            </x-subtitle>
+            <x-divider/>
+            <x-event-details
+                :event="event"
+                :venue="venue"
+            />
+        </x-card>
+        <x-card>
+            <x-title>
+                Stand
+            </x-title>
+            <x-subtitle>
+                View details about the concession stand you reserved a position at.
+            </x-subtitle>
+            <x-divider/>
+            <x-label>
+                Name
+            </x-label>
+            <x-text>
+                {{ reservation.stand_name }}
+            </x-text>
+            <x-divider/>
+            <div class="flex flex-row items-center justify-between">
+                <div>
+                    <x-label>
+                        Location
+                    </x-label>
+                    <x-text>
+                        {{ reservation.stand_location }}
+                    </x-text>
+                </div>
+                <div>
+                    <a
+                        href="#"
+                        target="_blank"
+                    >
+                        <x-icon-button>
+                            <MapIcon class="h-6 w-6 text-gray-500"/>
+                        </x-icon-button>
+                    </a>
+                </div>
+            </div>
+            <x-divider/>
+            <x-label>
+                Position
+            </x-label>
+            <x-text>
+                {{ reservation.position_type }}
+            </x-text>
+        </x-card>
+        <x-card>
+            <x-title>
+                Cancel
+            </x-title>
+            <x-subtitle>
+                Cancel your reservation if you no longer wish to attend the event.
+            </x-subtitle>
+            <x-divider/>
+            <x-button
+                class="text-red-600 border-red-600 hover:bg-red-600"
+                v-on:click="cancelReservation"
+            >
+                Cancel Your Reservation
+            </x-button>
+        </x-card>
     </volunteer-layout>
 </template>
 
 <script>
 import VolunteerLayout from "@/Layouts/VolunteerLayout";
-import EventCard from "@/Components/EventCard"
-import StandCard from "@/Components/StandCard"
-
+import XCard from "@/Components/Card";
+import XTitle from "@/Components/Title";
+import XSubtitle from "@/Components/Subtitle";
+import XDivider from "@/Components/Divider";
+import XText from "@/Components/Text";
+import XLabel from "@/Components/Label";
+import XButton from "@/Components/Button";
+import XIconButton from "@/Components/IconButton";
+import XEventDetails from "@/Components/EventDetails";
+import { MapIcon } from '@heroicons/vue/outline';
 export default {
     name: 'Show',
     components: {
         VolunteerLayout,
-        EventCard,
-        StandCard,
+        XCard,
+        XTitle,
+        XSubtitle,
+        XDivider,
+        XText,
+        XLabel,
+        XButton,
+        XIconButton,
+        XEventDetails,
+        MapIcon,
     },
     props: {
-        event: {
-            required: true,
-            type: Object,
-        },
-        venue: {
-            required: true,
-            type: Object,
-        },
-        reservation: {
-            required: true,
-            type: Object,
-        }
+        event: Object,
+        venue: Object,
+        reservation: Object,
     },
     data () {
         return {
@@ -44,6 +114,13 @@ export default {
             ],
         }
     },
+    methods: {
+        cancelReservation () {
+            this.$inertia.delete(this.route('volunteer.reservations.delete', {
+                id: this.reservation.id
+            }));
+        }
+    }
 }
 </script>
 
